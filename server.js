080637a3c2ab66f70c543417e1855647c61f7c8c
Hangsia Hong <44630728+hangsiahong@users.noreply.github.com>
2025-10-09 00:45:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const port = 8080;
+const port = 80;
 
 const server = http.createServer((req, res) => {
     fs.readFile('./index.html', (err, data) => {
